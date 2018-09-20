247f19e1d80adbae99830b919cf848146463fe86
kunalpareek <kunalp@outlook.in>
2018-09-21 01:11:57 +0530

    geth install script added

---
+++ install.sh
@@ -0,0 +1,5 @@
+wget https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.8.15-89451f7c.tar.gz
+tar -xzf geth-linux-amd64-1.8.15-89451f7c.tar.gz
+cp geth-linux-amd64-1.8.15-89451f7c/geth .
+rm -rf geth-linux-amd64-1.8.15-89451f7c
+mkdir data
